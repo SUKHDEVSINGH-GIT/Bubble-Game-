1d960bec1ad84a8aa0a745b7700a6402dc8ecd67
SUKHDEV SINGH LABANA <teddysingh1@gmail.com>
2021-07-24 15:57:13 +0530

    Bubble Game
    Functionalities : Bubble mouse over counter
    Bg change
    Result of score

---
+++ Prac em JS - GAME 1/script.js
@@ -0,0 +1,79 @@
+
+
+var count=0;
+
+function color()
+
+{
+if (count==20)
+{
+    alert("hurray u have won");
+}
+else { 
+count=count+1;
+document.getElementById("counter").innerHTML=count;
+document.body.style.backgroundColor ="red";
+}
+}
+function color1()
+{
+    if (count==20)
+    {
+        alert("hurray u have won");
+    }
+    else { 
+    count=count+1;
+    document.getElementById("counter").innerHTML=count;
+document.body.style.backgroundColor ="green";
+}
+}
+function color2()
+{
+    if (count==20)
+    {
+        alert("hurray u have won");
+    }
+    else { 
+    count=count+1;
+    document.getElementById("counter").innerHTML=count;
+document.body.style.backgroundColor ="blue";
+}
+}
+
+function color3()
+{
+    if (count==20)
+    {
+        alert("hurray u have won");
+    }
+    else { 
+    count=count+1;
+    document.getElementById("counter").innerHTML=count;
+document.body.style.backgroundColor ="orange";
+}
+}
+function color4()
+{
+    if (count==20)
+    {
+        alert("hurray u have won");
+    }
+    else { 
+    count=count+1;
+    document.getElementById("counter").innerHTML=count;
+document.body.style.backgroundColor ="pink";
+}
+}
+function color5()
+{
+    if (stop==20)
+    {
+        alert("hurray u have won");
+    }
+    else { 
+    count=count+1;
+    document.getElementById("counter").innerHTML=count;
+document.body.style.backgroundColor ="yellow";
+}
+}
+
